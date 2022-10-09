5b10eeaa58d27a431bce8bed17b2d3bbebf0a505
gunelmuradova <gunel.muradova.std@bhos.edu.az>
2022-10-09 11:51:50 -0700

    lab5

---
+++ src/main/java/org/example/springproj1App.java
@@ -0,0 +1,11 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class springproj1App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(springproj1App.class, args);
+    }
+
+}
